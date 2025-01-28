a22497227b3fc2e05a25f25680e6e12782ab1627
Andreas Gabrielsson <100436631+anga95@users.noreply.github.com>
2025-01-28 09:08:35 +0100

    Web project in progress

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,19 @@
+﻿using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers;
+
+public class CouponController : Controller
+{
+    private readonly ICouponService _couponService;
+    public CouponController(ICouponService couponService)
+    {
+        _couponService = couponService;
+    }
+    
+    // GET
+    public async Task<IActionResult> CouponIndex()
+    {
+        return View();
+    }
+}
